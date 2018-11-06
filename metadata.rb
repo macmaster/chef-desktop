36802f428784043cfa61a4b98914e8a105bef2dd
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-11-05 23:03:47 -0500

    Edit metadata and clean up unnecessary content

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,11 @@
 name 'chef-desktop'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
-description 'Installs/Configures chef-desktop'
-long_description 'Installs/Configures chef-desktop'
+maintainer 'Ronald Macmaster'
+maintainer_email 'ronnymacmaster@gmail.com'
+license 'Apache 2.0'
+description 'Personal Desktop Recipes'
+long_description 'Chef Recipes for my personal Debian desktops.'
 version '0.1.0'
 chef_version '>= 13.0'
 
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/chef-desktop/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/chef-desktop'
+issues_url 'https://github.com/ronny-macmaster/chef-desktop/issues'
+source_url 'https://github.com/ronny-macmaster/chef-desktop'
