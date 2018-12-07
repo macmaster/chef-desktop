c33f8beb8ff4aaf94f908f8fc86c473636714518
Ronald Macmaster <ronnymacmaster@gmail.com>
2018-12-07 00:49:42 -0500

    Install some vim packages. Manage them through root

--- recipes/vim.rb
+++ recipes/vim.rb
@@ -22,6 +22,36 @@ package 'vim' do
   action :upgrade
 end
 
-template "#{ENV['HOME']}/.vimrc" do
+# vim configuration
+VIM_HOME = ENV['HOME']
+VIM_USER = ENV['SUDO_USER'] || ENV['USER']
+VIM_PLUGINS = ".vim/pack/vim/start"
+VIM_RC = ".vimrc"
+
+# Create a .vimrc
+template File.join(VIM_HOME, VIM_RC) do
   source 'vim/.vimrc'
+  owner VIM_USER
+  group VIM_USER
+end
+
+# Create the plugins directory
+# Use the built in package system for vim 8+
+
+# Plugins are managed completely by chef (root:root)
+directory File.join(VIM_HOME, VIM_PLUGINS)
+
+# Install some vim packages
+plugin_repos = %w(
+  https://github.com/pangloss/vim-javascript.git
+  https://github.com/mxw/vim-jsx.git
+)
+
+plugin_repos.map do |repo|
+  plugin = File.basename(repo, '.git')
+  path = File.join(VIM_HOME, VIM_PLUGINS, plugin)
+
+  git path do
+    repository repo
+  end
 end
